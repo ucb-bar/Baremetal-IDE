0b082d0bba43ff8deaddd467d7b63fd5f40f6d1b
-T.K.- <t_k_233@outlook.com>
2023-08-21 14:15:30 -0700

    ADD: add htif example

---
+++ examples/htif_hello.c
@@ -0,0 +1,120 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+const char* get_march(size_t marchid) {
+  switch (marchid) {
+  case 1:
+    return "rocket";
+  case 2:
+    return "sonicboom";
+  case 5:
+    return "spike";
+  default:
+    return "unknown";
+  }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(int argc, char **argv) {
+  /* USER CODE BEGIN 1 */
+  
+	/* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+  
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
+    uint64_t marchid = READ_CSR("marchid");
+    const char* march = get_march(marchid);
+    printf("Hello world from core 0, a %s\n", march);
+
+    uint8_t *ptr = malloc(10);
+    printf("malloc: %p\n", ptr);
+    free(ptr);
+
+    return 0;
+		/* USER CODE END WHILE */
+	}
+
+	/* USER CODE BEGIN 3 */
+
+	/* USER CODE END 3 */
+}
+
+/*
+ * Main function for secondary harts
+ * 
+ * Multi-threaded programs should provide their own implementation.
+ */
+void __attribute__((weak, noreturn)) __main(void) {
+  while (1) {
+   asm volatile ("wfi");
+  }
+}
