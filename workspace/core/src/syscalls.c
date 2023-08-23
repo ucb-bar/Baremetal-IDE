a1c9f5d8d27663e747d9532d726049358ad7671b
-T.K.- <t_k_233@outlook.com>
2023-08-23 14:57:36 -0700

    FIX: re-adding missing codes

--- workspace/core/src/syscalls.c
+++ workspace/core/src/syscalls.c
@@ -1,29 +1,29 @@
-// #include "syscalls.h"
+#include "syscalls.h"
 
-// ssize_t _write(int fd, const void *ptr, size_t len) {
-//   HAL_UART_transmit(UART0, (uint8_t *)ptr, len, 100);
-//   return 0;
-// }
+ssize_t _write(int fd, const void *ptr, size_t len) {
+  HAL_UART_transmit(UART0, (uint8_t *)ptr, len, 100);
+  return 0;
+}
 
-// extern char __end[];
-// static char *curbrk = __end;
+extern char __end[];
+static char *curbrk = __end;
 
-// void *_sbrk(ptrdiff_t incr) {
-//   extern char __heap_end[];
-//   char *newbrk;
-//   char *oldbrk;
+void *_sbrk(ptrdiff_t incr) {
+  extern char __heap_end[];
+  char *newbrk;
+  char *oldbrk;
 
-//   oldbrk = curbrk;
-//   newbrk = oldbrk + incr;
-//   if (unlikely((newbrk < __end) || (newbrk >= __heap_end))) {
-//     errno = ENOMEM;
-//     return (void *)(-1);
-//   }
+  oldbrk = curbrk;
+  newbrk = oldbrk + incr;
+  if (unlikely((newbrk < __end) || (newbrk >= __heap_end))) {
+    errno = ENOMEM;
+    return (void *)(-1);
+  }
 
-//   curbrk = newbrk;
-//   return oldbrk;
-// }
-// int _fstat(int file, struct stat *st) {
-//   errno = EBADF;
-//   return -1;
-// }
+  curbrk = newbrk;
+  return oldbrk;
+}
+int _fstat(int file, struct stat *st) {
+  errno = EBADF;
+  return -1;
+}
