34d7e3f14f7edcf31705ec09aad194c638376206
-T.K.- <t_k_233@outlook.com>
2024-05-15 12:08:56 -0700

    ADD: add correct clock frequency

--- bsp/fe310/fe310.h
+++ bsp/fe310/fe310.h
@@ -124,7 +124,7 @@ typedef enum {
 /**
  * System Clock Configuration
  */
-#define HXTAL_FREQ          16000000                        /** crystal or external clock frequency in Hz */
+#define HXTAL_FREQ          18000000                        /** crystal or external clock frequency in Hz */
 #define SYS_CLK_FREQ        HXTAL_FREQ                      /** system clock frequency in Hz */
 #define MTIME_TIMEBASE      32768                           /** tick per milliseconds */
 #define MTIME_FREQ          MTIME_TIMEBASE
