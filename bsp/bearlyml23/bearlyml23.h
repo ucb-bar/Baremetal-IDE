e47dfb5de63191d063f77425581fbb8372f44ef5
Jason Kim <jasonskim007@bwrcrdsl-1.eecs.berkeley.edu>
2024-08-28 11:16:39 -0700

    update clock

--- bsp/bearlyml23/bearlyml23.h
+++ bsp/bearlyml23/bearlyml23.h
@@ -110,10 +110,11 @@ typedef enum {
 /**
  * System Clock Configuration
  */
-#define HXTAL_FREQ          100000000                        /** crystal or external clock frequency in Hz */
-#define SYS_CLK_FREQ        HXTAL_FREQ / 2                      /** system clock frequency in Hz */
+#define HXTAL_FREQ     100000000                   /** crystal or external clock frequency in Hz */
+#define SYS_CLK_FREQ  HXTAL_FREQ                /** system clock frequency in Hz */
 #define MTIME_TIMEBASE      1                               /** tick per milliseconds */
-#define MTIME_FREQ          100
+#define MTIME_FREQ    (SYS_CLK_FREQ / 100000)     // tick per milliseconds
+
 
 // weak definition of clock frequency
 __attribute__((weak)) uint64_t sys_clk_freq = SYS_CLK_FREQ;
