2535c23406e0b7635c53cc178d0aeb5d98665cfc
Lux <iansseijelly@berkeley.edu>
2024-10-29 18:04:31 -0700

    FIX: redefinition

--- driver/rocket-chip/rocketcore/rocketcore.h
+++ driver/rocket-chip/rocketcore/rocketcore.h
@@ -103,35 +103,6 @@ static inline void clear_irq(uint32_t IRQn) {
 }
 
 
-static inline size_t get_hart_id() {
-  return READ_CSR("mhartid");
-}
-
-static inline uint64_t get_cycles() {
-  return READ_CSR("mcycle");
-}
-
-static inline void disable_global_interrupt() {
-  CLEAR_CSR_BITS("mstatus", 1U << 3U);
-}
-
-static inline void enable_global_interrupt() {
-  SET_CSR_BITS("mstatus", 1U << 3U);
-}
-
-static inline void disable_irq(uint32_t IRQn) {
-  CLEAR_CSR_BITS("mie", 1U << IRQn);
-}
-
-static inline void enable_irq(uint32_t IRQn) {
-  SET_CSR_BITS("mie", 1U << IRQn);
-}
-
-static inline void clear_irq(uint32_t IRQn) {
-  CLEAR_CSR_BITS("mip", 1U << IRQn);
-}
-
-
 #ifdef __cplusplus
 }
 #endif
